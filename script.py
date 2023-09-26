522be87429cc7c255c25613c7d127076b6329e97
Ayman Mohamed <ayman.awad@adres.ae>
2023-09-26 20:25:49 +0400

    Initial commit

---
+++ script.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from IPython.display import display
+
+# Excel file path
+file_path = 'C:\Ayman\VS-servicenowreports\year\incident2023.xlsx'  # Update with your file path
+
+# Read the Excel file into a DataFrame
+df = pd.read_excel(file_path)
+
